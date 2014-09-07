fe93576839f2d71b1907484e2acf3218a9de88a7
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-07 06:15:05 +0000

    some changes
    git-svn-id: https://plugins.svn.wordpress.org/sis-social-share/trunk@983425 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ add_action('wp_footer', 'sis_social_share_activation');
 
 //add settings page to menu
 function sis_social_share_add_settings_page() {
-add_menu_page( __( 'Social Share Settings' ), __( 'Social Share' ), 'manage_options', 'settings', 'sis_social_share_settings_page');
+add_menu_page( __( 'Social Share Settings', 'socialsharebuttons' ), __( 'Social Share', 'socialsharebuttons' ), 'manage_options', 'settings', 'sis_social_share_settings_page');
 }
 add_action( 'admin_menu', 'sis_social_share_add_settings_page' );
 
@@ -172,54 +172,54 @@ function sis_social_share_settings_page() {
 				<!-- Option 1: Check Social Share -->
 				<tr valign="top">
 					<th scope="row">
-						<label><?php _e( 'Check Social Share you want to show' ); ?></label>
+						<label><?php _e( 'Check Social Share you want to show', 'socialsharebuttons' ); ?></label>
 					</th>
 					<td>
-						<input id="sis_social_share_settings[googleplus]" name="sis_social_share_settings[googleplus]" type="checkbox" value="true" <?php checked( 'true', $options['googleplus'] ); ?> />Google Plus<br>
+						<input id="sis_social_share_settings[googleplus]" name="sis_social_share_settings[googleplus]" type="checkbox" value="true" <?php checked( 'true', $options['googleplus'] ); ?> /><?php _e('Google Plus', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[facebook]" name="sis_social_share_settings[facebook]" type="checkbox" value="true" <?php checked( 'true', $options['facebook'] ); ?> />Facebook<br>
+						<input id="sis_social_share_settings[facebook]" name="sis_social_share_settings[facebook]" type="checkbox" value="true" <?php checked( 'true', $options['facebook'] ); ?> /><?php _e('Facebook', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[twitter]" name="sis_social_share_settings[twitter]" type="checkbox" value="true" <?php checked( 'true', $options['twitter'] ); ?> />Twitter<br>
+						<input id="sis_social_share_settings[twitter]" name="sis_social_share_settings[twitter]" type="checkbox" value="true" <?php checked( 'true', $options['twitter'] ); ?> /><?php _e('Twitter', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[digg]" name="sis_social_share_settings[digg]" type="checkbox" value="true" <?php checked( 'true', $options['digg'] ); ?> />Digg<br>
+						<input id="sis_social_share_settings[digg]" name="sis_social_share_settings[digg]" type="checkbox" value="true" <?php checked( 'true', $options['digg'] ); ?> /><?php _e('Digg', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[delicious]" name="sis_social_share_settings[delicious]" type="checkbox" value="true" <?php checked( 'true', $options['delicious'] ); ?> />Delicious<br>
+						<input id="sis_social_share_settings[delicious]" name="sis_social_share_settings[delicious]" type="checkbox" value="true" <?php checked( 'true', $options['delicious'] ); ?> /><?php _e('Delicious', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[stumbleupon]" name="sis_social_share_settings[stumbleupon]" type="checkbox" value="true" <?php checked( 'true', $options['stumbleupon'] ); ?> />Stumbleupon<br>
+						<input id="sis_social_share_settings[stumbleupon]" name="sis_social_share_settings[stumbleupon]" type="checkbox" value="true" <?php checked( 'true', $options['stumbleupon'] ); ?> /><?php _e('Stumbleupon', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[linkedin]" name="sis_social_share_settings[linkedin]" type="checkbox" value="true" <?php checked( 'true', $options['linkedin'] ); ?> />Linkedin<br>
+						<input id="sis_social_share_settings[linkedin]" name="sis_social_share_settings[linkedin]" type="checkbox" value="true" <?php checked( 'true', $options['linkedin'] ); ?> /><?php _e('Linkedin', 'socialsharebuttons'); ?><br>
 
-						<input id="sis_social_share_settings[pinterest]" name="sis_social_share_settings[pinterest]" type="checkbox" value="true" <?php checked( 'true', $options['pinterest'] ); ?> />Pinterest<br>
+						<input id="sis_social_share_settings[pinterest]" name="sis_social_share_settings[pinterest]" type="checkbox" value="true" <?php checked( 'true', $options['pinterest'] ); ?> /><?php _e('Pinterest', 'socialsharebuttons'); ?><br>
 						
-						<p class="description"><?php _e( 'Check Social Share you want to show' ); ?></p>
+						<p class="description"><?php _e( 'Check Social Share you want to show', 'socialsharebuttons' ); ?></p>
 					</td>
 				</tr>
 				<!-- Option 2: Check Social Share Position -->
 				<tr valign="top">
 					<th scope="row">
-						<label for="sis_social_share_settings[show_button]"><?php _e( 'Show Social Share' ); ?></label>
+						<label for="sis_social_share_settings[show_button]"><?php _e( 'Show Social Share', 'socialsharebuttons' ); ?></label>
 					</th>
 					<td>
 						<input type="radio" name="sis_social_share_settings[show_button]" value="left" <?php checked( $options['show_button'], 'left' ); ?> />Left<br />
 
 						<input type="radio" name="sis_social_share_settings[show_button]" value="right" <?php checked( $options['show_button'], 'right' ); ?> />Right<br />
 
-						<p class="description"><?php _e( 'Choose where you want to show buttons left or right side of your site.' ); ?></p>
+						<p class="description"><?php _e( 'Choose where you want to show buttons left or right side of your site.', 'socialsharebuttons' ); ?></p>
 					</td>
 				</tr>
 				<!-- Option 3: Social Share Position from top-->
 				<tr valign="top">
                     <th scope="row">
-                        <label for="sis_social_share_settings[button_top]"><?php _e('Input option Example') ?></label>
+                        <label for="sis_social_share_settings[button_top]"><?php _e('Input option Example', 'socialsharebuttons') ?></label>
                     </th>
                     <td>
                         <input type="text" placeholder="100px" name="sis_social_share_settings[button_top]" value="<?php esc_attr_e($options['button_top']); ?>" class="">
 
-                        <p class="description"><?php _e('Write button position from top in pixels. Example: 100px') ?></p>
+                        <p class="description"><?php _e('Write button position from top in pixels. Example: 100px', 'socialsharebuttons') ?></p>
                     </td>
                 </tr>
 			</table>
-			<p class="submit"><input type="submit" value="<?php _e('Save Changes') ?>" class="button button-primary" id="submit" name="submit"></p>
+			<p class="submit"><input type="submit" value="<?php _e('Save Changes', 'socialsharebuttons') ?>" class="button button-primary" id="submit" name="submit"></p>
 		</form>
 
 	</div><!-- END wrap -->
